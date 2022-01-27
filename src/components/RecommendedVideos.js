04f93ea39a44b742b151ff1d0e6a3f2f6b722dce
samsonajulor <samson.ajulor@decagon.dev>
2022-01-27 14:06:41 +0100

    replaced the dynamic data with dummy data

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -14,7 +14,7 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
         <VideoCard
           title='Become a Web Developer in 10 minutes | 2019/2020'
@@ -22,7 +22,7 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
         <VideoCard
           title='Become a Web Developer in 10 minutes | 2019/2020'
@@ -30,7 +30,7 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
         <VideoCard
           title='Become a Web Developer in 10 minutes | 2019/2020'
@@ -38,7 +38,7 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
         <VideoCard
           title='Become a Web Developer in 10 minutes | 2019/2020'
@@ -46,7 +46,7 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
         <VideoCard
           title='Become a Web Developer in 10 minutes | 2019/2020'
@@ -54,7 +54,7 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
         <VideoCard
           title='Become a Web Developer in 10 minutes | 2019/2020'
@@ -62,9 +62,8 @@ function RecommendedVideos() {
           timestamp='3 days ago'
           channelImage='https://scontent-cpt1-1.xx.fbcdn.net/v/t1.6435-9/127929844_192909205774620_4852570612652802182_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHzoiPboR1R3cVDHmZA5Bdbw52vDjWhjlTDna8ONaGOVH2G6lM8WdjU-8LVh4MTzwar5p72xiBhQxZhsorFEOnw&_nc_ohc=40QHxjZz3qAAX90Qh8h&_nc_ht=scontent-cpt1-1.xx&oh=00_AT-i2dOk5GKmm2BiKUn42RX4pdfJfGlzSMXrRThcUpP_QA&oe=62190A3F'
           channel='Sonny Sangha'
-          image='https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?'
+          image='https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2dyYXBoaWNzLWNhcmQtdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fSwidG9Gb3JtYXQiOiJhdmlmIn19'
         />
-  
       </div>
     </div>
   )
